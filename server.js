17b640320eba5e958a72f241c1f53cb2fe79813d
Julio Marroquín <julio.marroquin.SubAVH@avianca.com>
2018-02-28 10:27:19 -0600

    second commit

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 8000;
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
 var passport = require('passport');
 
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 require('./app/routes.js')(app, passport);
 app.set('view engine', 'ejs');
-app.listen(port);
-console.log('Server running on port: ' + port);
+app.listen(port,ip);
+console.log('Server running on port: ' + port,ip);
+module.exports = app;
