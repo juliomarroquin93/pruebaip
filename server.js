92de45dd9896fddaae8a11ef6d46a9e30bebd635
Julio Marroquín <julio.marroquin.SubAVH@avianca.com>
2018-02-28 10:56:55 -0600

    server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 var passport = require('passport');
 
 app.use(passport.initialize());
